0f9281abd15dbadcf0eeb135699e7ca2b715bcb4
Hamssatou Almahamoudou Maiga <hmaiga@smith.edu>
2023-04-24 13:39:52 -0400

    Now user can return bike successfully

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,17 +1,25 @@
 class RentalsController < ApplicationController
     def new
+      @bike= Bike.find_by_identifier(params[:format])
     end
   
     def create
       @rental = Rental.new(rental_params)
       puts @rental
-      if @rental.save
-        redirect_to ('/bikes/unlock'), notice: "Bike rented successfully!"
+      if @rental.save 
+        @bikes= Bike.find_by_identifier(params[:format])
+        $s= @bikes.current_station_id
+        @bikes.update({current_station_id: nil }) 
+        redirect_to (bikes_unlock_path(params[:format])), notice: "Bike rented successfully!"
       else
         render :new
       end
     end
- 
+    def return
+      @bikes= Bike.find_by_identifier(params[:format])
+      @bikes.update({current_station_id: $s })
+      redirect_to '/bikes/return'
+    end
   
     private
   
--- config/routes.rb
+++ config/routes.rb
@@ -22,15 +22,17 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#menu', as: 'user'
   get "user/show"
+  get 'return', to: 'rentals#return'
   
   # payment page info
   get "payment", to:'payment#new' # testing
   get "bikes", to: "bikes#index" # testing
-
+  get '/bikes/return', to: "bikes#return"
   match'ride',to:"bikes#index", via: :get
   resources :bikes, only: [:index]
   resources :stations, only: [:main, :show]
   resources :users, except: [:new]
   resources :rentals, only: [:new, :create]
+  resources :rentals, only: [:return]
 
 end
