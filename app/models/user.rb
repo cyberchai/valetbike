c8dd2544cd83fb561a5f79e5d3c861a25fbdc8bc
yina <ybao81@smith.edu>
2023-05-01 18:36:24 -0400

    migration issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_secure_password
-    
-    validates_uniqueness_of :email
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
 
-    validates_uniqueness_of :phone_num
+    validates_uniqueness_of :email
 end
